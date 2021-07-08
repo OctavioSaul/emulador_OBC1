5648819942de446855659812a40a5677b0567983
Saúl  Pérez <54450000+OctavioSaul@users.noreply.github.com>
2021-07-08 16:50:13 -0500

    Add files via upload

---
+++ masterI2C.py
@@ -0,0 +1,81 @@
+import smbus
+import time
+import math
+from struct import pack, unpack
+
+bus = smbus.SMBus(1)
+time.sleep(1)
+
+def sendData(slaveAddress, data):
+        #pasar a binario
+        #intsOfData = list(map(ord, data))
+    bus.write_i2c_block_data(slaveAddress, 0xFF,data)
+    print("comando enviado",data)
+def readData(slaveAddress,reg):
+    bytes=bus.read_i2c_block_data(slaveAddress,reg,16)
+    return bytes
+def llenar_comando(cont,send_list):
+    #tipo comando
+    send_list[0]=4
+    #llenar cero espacios vacios
+    for i in range(4, 14):
+      send_list[i]=0
+    #numero paquete a pedir
+    send_list[1]=(cont & (0xFF<<16))>>16
+    send_list[2]=(cont & (0xFF<<8))>>8
+    send_list[3]=(cont & (0xFF))
+    send_list[14]=sum(send_list[:14])&0xFF
+    return send_list
+valido=False
+check_sum=0
+total=0
+print("inicio")
+#comando pedir foto a OBC2
+send_list= [3] + [1]*13 + [16] 
+sendData(0x03,send_list)
+while valido== False:
+   time.sleep(1)
+   #leer tamano foto
+   bytes=readData(0x03, 0xFF)
+   print(bytes)
+  #comprobar si es comando
+   if bytes[0]==6:
+      #si encontro la foto
+      if bytes[1]==1:
+         check_sum=sum(bytes[:15])&0xFF
+         print(check_sum)
+         #comprobar checksum
+         if check_sum==bytes[15]:
+            #convertir numero de paquetes
+            total=math.ceil(sum([bytes[5-i]<<8*i for i in range(4)])/14)
+            print("total de paquetes: ", total)
+            valido=True
+         else:
+            print("volver a mandar")
+            sendData(0x03,send_list)
+valido=False
+cont=0
+#pedir primer paquete
+send_list=llenar_comando(cont,send_list)
+sendData(0x03,send_list)
+print("enviando paquetes...")
+while valido== False:
+   time.sleep(1)
+   #leer paquete
+   bytes=readData(0x03, 0xFF)
+   #es un comando?
+   if bytes[0]==6:
+      check_sum=sum(bytes[:16])&0xFF
+      #check sum es correcto?
+      if check_sum==bytes[15]:
+         cont+=1
+         print(bytes)
+         if cont>=total:
+            valido=True
+         else:
+            send_list=llenar_comando(cont,send_list)
+            sendData(0x03,send_list)
+      else:
+         sendData(0x03,send_list)
+
+
